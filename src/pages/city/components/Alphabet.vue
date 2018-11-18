537dfad6b3cd5ebceb9585599ac5d58e9cf8843b
Zhuanglijuan <1330129868@qq.com>
2018-11-18 19:52:30 +0800

    ajax city

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,19 +1,17 @@
 <template>
     <ul class="list">
-      <li class="item">A</li>
-      <li class="item">A</li>
-      <li class="item">A</li>
-      <li class="item">A</li>
-      <li class="item">A</li>
-      <li class="item">A</li>
-      <li class="item">A</li>
-      <li class="item">A</li>
+      <li class="item" v-for="(item,key) of cities" :key="key">
+        {{key}}
+      </li>
     </ul>
 </template>
 
 <script>
 export default {
-  name: 'CityAplhabet'
+  name: 'CityAplhabet',
+  props: {
+    cities: Object
+  }
 }
 </script>
 
